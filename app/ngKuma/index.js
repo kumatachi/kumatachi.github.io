630cfc6353662734ed126e952afaa71fdba5300c
Bennilyn <bennilynq@gmail.com>
2014-11-13 17:55:37 -0600

    Added projects apge

--- app/ngKuma/index.js
+++ app/ngKuma/index.js
@@ -5,5 +5,6 @@
 // list all the modules of OpportunityFinder
 require('./topBar/topBar.js');
 require('./home/home.js');
+require('./projects/projects.js');
 
 module.exports = require('./module.js');
