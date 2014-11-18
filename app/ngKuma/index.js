aa9f53dfb3656a7aa2ec9f543a090f03f222d35e
August Aiou <hachigatsuaiou@gmail.com>
2014-11-18 00:21:55 -0600

    style update

--- app/ngKuma/index.js
+++ app/ngKuma/index.js
@@ -3,7 +3,7 @@
  */
 
 // list all the modules of OpportunityFinder
-require('./topBar/topBar.js');
+//require('./topBar/topBar.js');
 require('./home/home.js');
 require('./projects/projects.js');
 require('./contact/contact.js');
